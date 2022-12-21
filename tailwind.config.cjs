ee515f8f574a9566c561e887e086c21b0d9b2618
Prakash Sewani <adamjones2227@gmail.com>
2022-12-21 21:44:42 +0530

    addadded a navbar, component configs and themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    borderColor: {
+      black: "#000000",
+    },
+    extend: {
+      colors: {
+        blue: "#117eff",
+        yellow: "#f5b127",
+        pink: "#f64a89",
+        black: "#000000",
+      },
+    },
   },
   plugins: [],
 };
