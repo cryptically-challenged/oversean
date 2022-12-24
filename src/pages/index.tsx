8f52e896a5d35f3a056d2a71374260407573ec7b
Prakash Sewani <adamjones2227@gmail.com>
2022-12-24 11:56:10 +0530

    skeleton ready just need to fill content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,39 +4,24 @@ import Link from "next/link";
 import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [disabled1, setDisabled1] = useState<boolean>(true);
+  const [disabled2, setDisabled2] = useState<boolean>(true);
 
-  const [disabled1,setDisabled1]=useState<boolean>(true);
-  const [disabled2,setDisabled2]=useState<boolean>(true);
-  
   return (
     <>
-      {/* <button className="btn shadow-solid">content</button>
-      <br />
-      <br />
-      <input
-        type="text"
-        placeholder="Enter your Email"
-        className="input-text shadow-solid"
-      />
-      <br />
-      <br />
-      <button type="submit" className="btn-submit shadow-solid-curve">
-        submit or signin
-      </button>
-      <br />
-      <br />
-      <div className="card shadow-solid-div"></div> */}
-
-<div className="flex justify-center">
+      {/* Navigation Bar */}
+      <div className="flex justify-center">
         <div className="w-5/6">
-          <div className="flex justify-between ... py-3">
-            <div className="">
+          <div className="... flex justify-between py-3">
+            <div className=""></div>
 
-            </div>
-
-            <div className="flex space-x-12 ml-auto mr-32 ...">
-              <button className="text-xl" disabled={disabled1}>Placeholder</button>
-              <button className="text-xl" disabled={disabled2}>Placeholder</button>
+            <div className="... ml-auto mr-20 flex space-x-12">
+              <button className="text-xl" disabled={disabled1}>
+                Placeholder
+              </button>
+              <button className="text-xl" disabled={disabled2}>
+                Placeholder
+              </button>
             </div>
 
             <div className="">
@@ -47,6 +32,31 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
+      {/* Main Page */}
+      <div className="my-10 flex justify-center">
+        <div className="flex w-5/6">
+          <div className="w-1/2 mr-2.5">
+            <div className="h-1/2 w-full bg-blue"></div>
+            <div className="card shadow-solid-curve h-1/2 w-full"></div>
+          </div>
+          <div className="w-1/2 ml-2.5">
+            <div className="card shadow-solid-div h-screen"></div>
+          </div>
+        </div>
+      </div>
+      {/* Information Page */}
+      <div className="my-20 flex justify-center">
+        <div className="flex h-96 w-5/6 justify-between">
+          <div className="card shadow-solid-div h-6/7 w-1/3"></div>
+          <div className="card shadow-solid-div h-6/7 mr-5 ml-5 w-1/3"></div>
+          <div className="card shadow-solid-div h-6/7 w-1/3"></div>
+        </div>
+      </div>
+      {/* Contact Us Page */}
+      <div className="my-20 flex justify-center">
+        <div className="card shadow-solid-curve h-96 w-5/6"></div>
+      </div>
+      {/* Footer */}
     </>
   );
 };
